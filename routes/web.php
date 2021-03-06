66b102fb88a2714e4a64f51a4a071c509543b935
Mara Lupano <mara@salviharps.com>
2021-03-06 18:01:40 +0100

    new registration

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,23 @@ Route::get('/sanctum/token', function (Request $request) {
 
     return $user->createToken($request->device_name)->plainTextToken;
 });
+
+Route::get('/sanctum/register', function (Request $request) {
+
+        $attr = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $attr['name'],
+            'password' => bcrypt($attr['password']),
+            'email' => $attr['email']
+        ]);
+
+        return $this->success([
+            'token' => $user->createToken('API Token')->plainTextToken
+        ]);
+
+});
